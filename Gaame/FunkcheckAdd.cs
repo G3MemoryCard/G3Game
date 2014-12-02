d94cde59da56ac763938bc12b30d15b3b115738f
TheBaaron <grundstrom5@hotmail.com>
2014-12-02 11:55:06 +0100

    Update FunkAddCheck

---
+++ Gaame/FunkcheckAdd.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gaame
+{
+    class FunkcheckAdd
+    {
+
+        public bool Checkpic()
+        {
+
+            //if(Picturbox[0].image.tag == Picturbox[1].image.tag) return true;
+            return false;
+        }
+
+        public void AddScore(string player, int PairCount)
+        {
+            //List Heloo = new List<string, int>();
+            //heloo<player, playerscore>
+            int playerScore = 0; //Nuvarande poängen
+            int score = 5; //Poängen som ska delas ut
+            score = score * PairCount; // Score gånger antal par för combo poäng
+            playerScore = score + playerScore; // Score + nuvarande score för att få ut den nya poängen.
+            //lägg till poängen i listan under "player, int".
+        }
+    }
+}
