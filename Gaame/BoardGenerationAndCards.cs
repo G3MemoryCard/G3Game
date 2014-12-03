30e8517275d42237ce0246ec61cdbdd836c71337
Andrei <goldhead-00@hotmail.com>
2014-12-03 14:11:48 +0100

    Random

--- Gaame/BoardGenerationAndCards.cs
+++ Gaame/BoardGenerationAndCards.cs
@@ -12,6 +12,21 @@ namespace Gaame
 {
     public static class BoardGeneration
     {
+        //Shuffles list
+        public static void Shuffle<T>(this IList<T> list)
+        {
+            int n = list.Count;
+            Random rnd = new Random();
+            while (n > 1)
+            {
+                int k = (rnd.Next(0, n) % n);
+                n--;
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
+
         //Generate tag list with two of each tag
         static List<int> GenerateTags(int pairs)
         {
@@ -23,6 +38,9 @@ namespace Gaame
                 tagList.Add(i);
                 i++;
             }
+            Shuffle<int>(tagList);
+            foreach (int a in tagList)
+                Console.WriteLine(a);
             return tagList;
         }
 
@@ -32,6 +50,7 @@ namespace Gaame
             list.RemoveAt(index);
         }
 
+        //Generates the board and returns an array of playcards
         public static PlayCard[] GenerateBoard(int x, SplitContainer container)
         {
             int maxBoardSize = 500;
@@ -54,6 +73,7 @@ namespace Gaame
             {
                 cardSize = cardMaxSize;
             }
+
             //Generate tag list
             List<int> tagList = GenerateTags(x / 2);
 
@@ -80,13 +100,11 @@ namespace Gaame
                 container.Panel1.Controls.Add(pic1);
                 PlayCard Card = new PlayCard(pic1);
 
-                //Add a random tag from the tag list to the card
-                Random random = new Random();
-                int curTag = random.Next(tagList.Count - 1);
-                Card.Tag = tagList[curTag];
+                //Add a tag from the random tag list to the card
+                Card.Tag = tagList[0];
 
                 //Removes the used tag from the list
-                tagList.RemoveAt(curTag);
+                tagList.RemoveAt(0);
 
                 Cards[i] = Card;
 
@@ -112,9 +130,8 @@ namespace Gaame
             AllowedToTurn = true;
             this.Pic = pic;
             this.Pic.Click += new EventHandler(Pic_Click);
-            FrontImage = ThemeArrays.Theme[Tag, 0];
+
             BackImage = ThemeArrays.Back[0];
-            Console.WriteLine(Tag);
             this.Pic.Image = BackImage;
         }
 
@@ -128,6 +145,7 @@ namespace Gaame
                 TurnUpCard();
             }
         }
+        //Turns the card to diplay the picture up
         public void TurnUpCard()
         {
             FrontImage = ThemeArrays.Theme[Tag, 0];
@@ -137,6 +155,7 @@ namespace Gaame
             Pic.Image = FrontImage;
         }
 
+        //Turns the card to display the back
         public void TurnDownCard()
         {
             BackImage = ThemeArrays.Back[0];
@@ -148,7 +167,7 @@ namespace Gaame
 
     }
 
-    //An array to get fronts and backs for cards depening on tags
+    //An array to get fronts and backs for cards depening on tags and settings
     public static class ThemeArrays
     {
         public static Bitmap[,] Theme { get; set; }
@@ -174,11 +193,43 @@ namespace Gaame
             Back[0] = Properties.Resources.CardBackGoldBlue;
         }
     }
+
+    //Keeps track of the generated cards
     public static class CardList
     {
-        //public static int SearchForPairs()
-        //{
-        //    
-        //}
+        public static PlayCard[] Cards { get; set; }
+
+        //Creates the card list
+        public static void Create(PlayCard[] cards)
+        {
+            Cards = cards;
+        }
+
+        //Looks through list to find a pair and returns the two card tags as an array
+        //If there is not two cards fliped it returns the values 100 and 101
+        public static int[] SearchForPairs()
+        {
+            int flipCount = 0;
+            int[] cardTags = new int[2];
+            foreach(PlayCard card in Cards)
+            {
+                if(card.Turned == true)
+                {
+                    flipCount++;
+                    cardTags[0] = card.Tag;
+                    if(flipCount == 1)
+                    {
+                        cardTags[1] = card.Tag;
+                    }
+                    
+                    if(flipCount == 2)
+                    {
+                        return cardTags;
+                    }
+                }
+            }
+            return new int[]{100,101};
+        }
     }
+    
 }
