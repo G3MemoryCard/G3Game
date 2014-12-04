73d8c73ba5e819dc289c51d1e1862ba27e21787e
Zevered <stefansipola@hotmail.com>
2014-12-04 14:14:28 +0100

    Update startscreen

--- Gaame/Properties/Resources.Designer.cs
+++ Gaame/Properties/Resources.Designer.cs
@@ -113,6 +113,16 @@ namespace Gaame.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Banner_ {
+            get {
+                object obj = ResourceManager.GetObject("Banner_", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Board_DarkBrador {
             get {
                 object obj = ResourceManager.GetObject("Board_DarkBrador", resourceCulture);
--- Gaame/StartScreen.Designer.cs
+++ Gaame/StartScreen.Designer.cs
@@ -36,7 +36,6 @@
             this.textBoxPairs = new System.Windows.Forms.TextBox();
             this.comboBoxTheme = new System.Windows.Forms.ComboBox();
             this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
             this.comboBoxDecks = new System.Windows.Forms.ComboBox();
             this.label6 = new System.Windows.Forms.Label();
             this.textBoxTimer = new System.Windows.Forms.TextBox();
@@ -44,12 +43,14 @@
             this.button3 = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.listView1 = new System.Windows.Forms.ListView();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(315, 293);
+            this.button1.Location = new System.Drawing.Point(324, 331);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(95, 31);
             this.button1.TabIndex = 0;
@@ -60,18 +61,20 @@
             // label2
             // 
             this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(10, 196);
+            this.label2.Location = new System.Drawing.Point(63, 268);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(195, 19);
+            this.label2.Size = new System.Drawing.Size(137, 19);
             this.label2.TabIndex = 2;
-            this.label2.Text = "How many cards (pairs):";
+            this.label2.Text = "Number of pairs:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(130, 220);
+            this.label3.Location = new System.Drawing.Point(133, 292);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(67, 19);
             this.label3.TabIndex = 3;
@@ -79,7 +82,7 @@
             // 
             // textBoxPairs
             // 
-            this.textBoxPairs.Location = new System.Drawing.Point(196, 198);
+            this.textBoxPairs.Location = new System.Drawing.Point(206, 267);
             this.textBoxPairs.MaxLength = 3;
             this.textBoxPairs.Name = "textBoxPairs";
             this.textBoxPairs.Size = new System.Drawing.Size(100, 20);
@@ -95,7 +98,7 @@
             "Social Icons",
             "Smileys",
             "Flags"});
-            this.comboBoxTheme.Location = new System.Drawing.Point(196, 223);
+            this.comboBoxTheme.Location = new System.Drawing.Point(206, 292);
             this.comboBoxTheme.Name = "comboBoxTheme";
             this.comboBoxTheme.Size = new System.Drawing.Size(100, 21);
             this.comboBoxTheme.TabIndex = 6;
@@ -103,22 +106,13 @@
             // label4
             // 
             this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
             this.label4.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(134, 246);
+            this.label4.Location = new System.Drawing.Point(137, 318);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(63, 19);
             this.label4.TabIndex = 7;
             this.label4.Text = "Decks:";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(88, 16);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(192, 19);
-            this.label5.TabIndex = 8;
-            this.label5.Text = "Welcome to G3 Memory!";
             // 
             // comboBoxDecks
             // 
@@ -130,7 +124,7 @@
             "Black & Orange",
             "Brown & Beige",
             "Gold & Purple"});
-            this.comboBoxDecks.Location = new System.Drawing.Point(196, 249);
+            this.comboBoxDecks.Location = new System.Drawing.Point(206, 318);
             this.comboBoxDecks.Name = "comboBoxDecks";
             this.comboBoxDecks.Size = new System.Drawing.Size(100, 21);
             this.comboBoxDecks.TabIndex = 9;
@@ -138,8 +132,9 @@
             // label6
             // 
             this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
             this.label6.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(86, 271);
+            this.label6.Location = new System.Drawing.Point(86, 343);
             this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(114, 19);
@@ -148,7 +143,7 @@
             // 
             // textBoxTimer
             // 
-            this.textBoxTimer.Location = new System.Drawing.Point(196, 274);
+            this.textBoxTimer.Location = new System.Drawing.Point(206, 343);
             this.textBoxTimer.Margin = new System.Windows.Forms.Padding(2);
             this.textBoxTimer.MaxLength = 5;
             this.textBoxTimer.Name = "textBoxTimer";
@@ -160,7 +155,7 @@
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold);
-            this.button2.Location = new System.Drawing.Point(315, 127);
+            this.button2.Location = new System.Drawing.Point(324, 186);
             this.button2.Margin = new System.Windows.Forms.Padding(2);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(68, 30);
@@ -172,7 +167,7 @@
             // button3
             // 
             this.button3.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold);
-            this.button3.Location = new System.Drawing.Point(315, 162);
+            this.button3.Location = new System.Drawing.Point(324, 220);
             this.button3.Margin = new System.Windows.Forms.Padding(2);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(68, 31);
@@ -183,11 +178,22 @@
             // 
             // listView1
             // 
-            this.listView1.Location = new System.Drawing.Point(88, 52);
+            this.listView1.Location = new System.Drawing.Point(106, 119);
             this.listView1.Name = "listView1";
             this.listView1.Size = new System.Drawing.Size(200, 132);
             this.listView1.TabIndex = 0;
             this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::Gaame.Properties.Resources.Banner_;
+            this.pictureBox1.Location = new System.Drawing.Point(44, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(348, 110);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 15;
+            this.pictureBox1.TabStop = false;
             // 
             // StartScreen
             // 
@@ -195,14 +201,14 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(425, 336);
+            this.ClientSize = new System.Drawing.Size(434, 381);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.listView1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.textBoxTimer);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.comboBoxDecks);
-            this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.comboBoxTheme);
             this.Controls.Add(this.textBoxPairs);
@@ -215,6 +221,7 @@
             this.Text = "G3 Memory";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form2_FormClosing);
             this.Load += new System.EventHandler(this.StartScreen_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -228,7 +235,6 @@
         private System.Windows.Forms.TextBox textBoxPairs;
         private System.Windows.Forms.ComboBox comboBoxTheme;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
         private System.Windows.Forms.ComboBox comboBoxDecks;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.TextBox textBoxTimer;
@@ -236,6 +242,7 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
