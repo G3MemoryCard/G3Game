a3fad254f254f2353288de745d52e399ecf278fd
JalleHall <blafarg@hotmail.com>
2014-12-04 09:50:19 +0100

    New classfile GameMaster.cs

---
+++ Gaame/GameMaster.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gaame
+{
+    class GameMaster
+    {
+        //Active player
+
+
+
+        //Check Pic
+
+
+
+        //Add Score
+
+
+
+        //Tidklocka
+
+
+
+        //Next Player
+
+
+
+
+
+    }
+}
