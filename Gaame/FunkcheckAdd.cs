eec9b2a615365e1bec40d9b5463b0aa14e160182
TheBaaron <grundstrom5@hotmail.com>
2014-12-04 09:57:28 +0100

    update

--- Gaame/FunkcheckAdd.cs
+++ Gaame/FunkcheckAdd.cs
@@ -19,11 +19,11 @@ namespace Gaame
 
         public void AddScore(int player, int PairCount)
         {
-            int playerScore = PlayerList.list[player].Score; //Nuvarande poängen
-            int score = 5; //Poängen som ska delas ut
-            score = score * PairCount; // Score gånger antal par för combo poäng
-            playerScore = score + playerScore; // Score + nuvarande score för att få ut den nya poängen.
-            PlayerList.list[player].Score = playerScore;
+            int playerScore = PlayerList.list[player].Score; //Check the score for the current player.
+            int score = 5; //Points for a pair.
+            score = score * PairCount; //Calculate the combo for pairs.
+            playerScore = score + playerScore; //Add playerscore to the new score.
+            PlayerList.list[player].Score = playerScore; //Put back the score to the player.
         }
     }
 }
