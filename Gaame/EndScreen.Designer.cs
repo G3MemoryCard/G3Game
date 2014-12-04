1764ddcff4caba918e9ce5f18f4a968083298881
Zevered <stefansipola@hotmail.com>
2014-12-04 16:20:00 +0100

    Update Endscreen

--- Gaame/EndScreen.Designer.cs
+++ Gaame/EndScreen.Designer.cs
@@ -28,19 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(EndScreen));
             this.btnPlayAgain = new System.Windows.Forms.Button();
             this.btnExit = new System.Windows.Forms.Button();
-            this.labelCongratulations = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.btnScrBoard = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // btnPlayAgain
             // 
-            this.btnPlayAgain.Location = new System.Drawing.Point(127, 134);
-            this.btnPlayAgain.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnPlayAgain.Location = new System.Drawing.Point(115, 133);
+            this.btnPlayAgain.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnPlayAgain.Name = "btnPlayAgain";
-            this.btnPlayAgain.Size = new System.Drawing.Size(88, 32);
+            this.btnPlayAgain.Size = new System.Drawing.Size(66, 26);
             this.btnPlayAgain.TabIndex = 3;
             this.btnPlayAgain.Text = "&Play Again";
             this.btnPlayAgain.UseVisualStyleBackColor = true;
@@ -49,68 +51,74 @@
             // btnExit
             // 
             this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.btnExit.Location = new System.Drawing.Point(221, 134);
-            this.btnExit.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnExit.Location = new System.Drawing.Point(185, 133);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnExit.Name = "btnExit";
-            this.btnExit.Size = new System.Drawing.Size(88, 32);
+            this.btnExit.Size = new System.Drawing.Size(66, 26);
             this.btnExit.TabIndex = 4;
             this.btnExit.Text = "&Exit";
             this.btnExit.UseVisualStyleBackColor = true;
             this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
-            // 
-            // labelCongratulations
-            // 
-            this.labelCongratulations.AutoSize = true;
-            this.labelCongratulations.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelCongratulations.Location = new System.Drawing.Point(91, 31);
-            this.labelCongratulations.Name = "labelCongratulations";
-            this.labelCongratulations.Size = new System.Drawing.Size(205, 29);
-            this.labelCongratulations.TabIndex = 5;
-            this.labelCongratulations.Text = "Congratulations!";
             // 
             // textBox1
             // 
             this.textBox1.BackColor = System.Drawing.SystemColors.Control;
             this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(28, 73);
+            this.textBox1.Location = new System.Drawing.Point(22, 83);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(379, 56);
+            this.textBox1.Size = new System.Drawing.Size(326, 46);
             this.textBox1.TabIndex = 6;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // btnScrBoard
             // 
-            this.btnScrBoard.Location = new System.Drawing.Point(127, 171);
+            this.btnScrBoard.Location = new System.Drawing.Point(135, 163);
+            this.btnScrBoard.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnScrBoard.Name = "btnScrBoard";
-            this.btnScrBoard.Size = new System.Drawing.Size(134, 32);
+            this.btnScrBoard.Size = new System.Drawing.Size(100, 26);
             this.btnScrBoard.TabIndex = 7;
             this.btnScrBoard.Text = "View &Scoreboard";
             this.btnScrBoard.UseVisualStyleBackColor = true;
             this.btnScrBoard.Click += new System.EventHandler(this.btnScrBoard_Click);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = global::Gaame.Properties.Resources.Banner_Congrats;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(43, 5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(284, 73);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
             // EndScreen
             // 
             this.AcceptButton = this.btnPlayAgain;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.CancelButton = this.btnExit;
-            this.ClientSize = new System.Drawing.Size(419, 246);
+            this.ClientSize = new System.Drawing.Size(374, 211);
             this.ControlBox = false;
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.btnScrBoard);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.labelCongratulations);
             this.Controls.Add(this.btnExit);
             this.Controls.Add(this.btnPlayAgain);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "EndScreen";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "G3 Memory";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -120,9 +128,9 @@
 
         private System.Windows.Forms.Button btnPlayAgain;
         private System.Windows.Forms.Button btnExit;
-        private System.Windows.Forms.Label labelCongratulations;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button btnScrBoard;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
