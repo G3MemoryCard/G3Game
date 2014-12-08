da93d430deb977dd2d4e005ee8cdff998331d81e
Andrei <goldhead-00@hotmail.com>
2014-12-08 12:06:11 +0100

    a

--- Gaame/BoardGenerationAssist.cs
+++ Gaame/BoardGenerationAssist.cs
@@ -9,25 +9,29 @@ namespace Gaame
     public static class BoardGenerationAssist
     {
         static int singles { get; set; }
-        public static List<int> rectOptions { get; set; }
+        static List<int> rectOptions { get; set; }
         static int i { get; set; }
+        static decimal d { get; set; }
 
         public static void getColumnsFromPairs(int pairs)
         {
+            rectOptions = new List<int>();
             singles = pairs * 2;
 
             i = 1;
             while(i <= 12)
             {
-                if (((singles / i) % 1) == 0)
+                d = singles / i;
+                if ((d % 1) == 0)
                 {
+                    Console.WriteLine(d);
                     rectOptions.Add(i);
                 }
                 i++;
             }
 
-            foreach (int a in rectOptions)
-                Console.WriteLine(a);
+            //foreach (int a in rectOptions)
+                
             
         }
 
